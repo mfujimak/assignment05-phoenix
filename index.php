cea05c59efca4814255e5799c1358a1a23ec13d2
apple <apple@dhcp-10-5-12-137.wireless.rochester.edu>
2018-04-21 18:07:44 -0400

    login

--- index.php
+++ index.php
@@ -6,15 +6,25 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="css/styles.css">
 </head>
-<body> 
-	<h1> Disney World!</h1>
+<body class="z-pattern"> <!-- Z-pattern -->
 
-<header>
+
+<header class="fixed">
+	<div class="container">
+		<div class="primary">
+		<!-- Primary Optical Area -->
+	<a href="mickey.php"><img src="images/logo.jpg" alt="Mickey logo"></a>
+        </div>
+        <div class="secondary">
+    <a href="stich.php"><img src="images/logo.jpg" alt="Stich logo"></a>
+        </div>
+      </div>
 		
 </header>
 
 <main>
-
+	<!-- Z-pattern and h1 should be in the center -->
+<h1> Disney World!</h1>
 
 </main>
 
