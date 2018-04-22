d27ba7e150d2a18776feed428e0a482816fc84a9
apple <apple@dhcp-10-5-25-18.wireless.rochester.edu>
2018-04-22 19:27:13 -0400

    nav

---
+++ includes/nav.php
@@ -0,0 +1,17 @@
+
+<!-- Navigation bar -->
+
+<nav class="menu">
+    <div>
+      <ul>
+      	<li><a href="index.php">Home</a></li>
+        <li><a href="mickey.php">Mickey Mouse</a></li>
+        <li><a href="stich.php">Stich</a></li>
+        <li><a href="simba.php">Simba</a></li>
+        <li><a href="login.php">Log In</a></li>
+
+      </ul>
+    </div>
+</nav>
+
+<!-- End of Navigation bar -->
