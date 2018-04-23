4dd2eb23e2711eeda7b59dd4e42d3197ed1dab90
Valerie <xwang145@u.rochester.edu>
2018-04-23 02:47:18 -0400

    updated stylesheets

--- mickey.php
+++ mickey.php
@@ -13,7 +13,7 @@
 </head>
 
 
-<body>
+<body class="f-pattern">
   <header>
     <h1>Mickey Mouse</h1>
     <span>Assignment05</span>
@@ -24,39 +24,29 @@
    ?>  
  </header>
 
-
+<main>
+  <div class="container">
 
 	<h2>Origin</h2> 
-
-  <p>
-    Mickey Mouse was created as a replacement for Oswald the Lucky Rabbit, an earlier cartoon character created by the Disney studio for Charles Mintz, a film producer who distributed product through Universal Studios.[4] In the spring of 1928, with the series going strong, Disney asked Mintz for an increase in the budget. But Mintz instead demanded that Walt take a 20 percent budget cut, and as leverage, he reminded Disney that Universal owned the character, and revealed that he had already signed most of Disney's current employees to his new contract. Angrily, Disney refused the deal and returned to produce the final Oswald cartoons he contractually owed Mintz. Disney was dismayed at the betrayal by his staff but determined to restart from scratch. The new Disney Studio initially consisted of animator Ub Iwerks and a loyal apprentice artist, Les Clark, who together with Wilfred Jackson were among the few who remained loyal to Walt. One lesson Disney learned from the experience was to thereafter always make sure that he owned all rights to the characters produced by his company.
-  </p>
-
   <div>
 
-        <figure class="photo-frame">
+        <figure class="float">
           <img src="images/mouse1.png" alt="mickeymouse">
           <figcaption>Mickey</figcaption>
         </figure>
 
       </div>
-
+  <p>
+    Mickey Mouse was created as a replacement for Oswald the Lucky Rabbit, an earlier cartoon character created by the Disney studio for Charles Mintz, a film producer who distributed product through Universal Studios.[4] In the spring of 1928, with the series going strong, Disney asked Mintz for an increase in the budget. But Mintz instead demanded that Walt take a 20 percent budget cut, and as leverage, he reminded Disney that Universal owned the character, and revealed that he had already signed most of Disney's current employees to his new contract. Angrily, Disney refused the deal and returned to produce the final Oswald cartoons he contractually owed Mintz. Disney was dismayed at the betrayal by his staff but determined to restart from scratch. The new Disney Studio initially consisted of animator Ub Iwerks and a loyal apprentice artist, Les Clark, who together with Wilfred Jackson were among the few who remained loyal to Walt. One lesson Disney learned from the experience was to thereafter always make sure that he owned all rights to the characters produced by his company.
+  </p>
 
 
 
 	<h2>Appearance</h2>
 
-  <p> 
-    Throughout the earlier years, Mickey's design bore heavy resemblance to Oswald, save for the ears, nose, and tail.[12][13][14] Ub Iwerks designed Mickey's body out of circles in order to make the character simple to animate. Disney employees John Hench and Marc Davis believed that this design was part of Mickey's success as it made him more dynamic and appealing to audiences.
-    Mickey's circular design is most noticeable in his ears. In animation in the 1940s, Mickey's ears were animated in a more realistic perspective. Later, they were drawn to always appear circular no matter which way Mickey was facing. This made Mickey easily recognizable to audiences and made his ears an unofficial personal trademark. The circular rule later created a dilemma for toy creators who had to recreate a three-dimensional Mickey.
-    Besides Mickey's gloves and shoes, he typically wears only a pair of shorts with two large buttons in the front. Before Mickey was seen regularly in color animation, Mickey's shorts were either red or a dull blue-green. With the advent of Mickey's color films, the shorts were always red. When Mickey is not wearing his red shorts, he is often still wearing red clothing such as a red bandmaster coat
-  </p>
-
-   
-
   <div>
 
-        <figure class="photo-frame">
+        <figure class="float">
           <img src="images/mouse2.png" alt="mickeymouse">
           <figcaption>Mickey</figcaption>
         </figure>
@@ -64,27 +54,35 @@
 
       </div>
 
+  <p> 
+    Throughout the earlier years, Mickey's design bore heavy resemblance to Oswald, save for the ears, nose, and tail.[12][13][14] Ub Iwerks designed Mickey's body out of circles in order to make the character simple to animate. Disney employees John Hench and Marc Davis believed that this design was part of Mickey's success as it made him more dynamic and appealing to audiences.
+    Mickey's circular design is most noticeable in his ears. In animation in the 1940s, Mickey's ears were animated in a more realistic perspective. Later, they were drawn to always appear circular no matter which way Mickey was facing. This made Mickey easily recognizable to audiences and made his ears an unofficial personal trademark. The circular rule later created a dilemma for toy creators who had to recreate a three-dimensional Mickey.
+    Besides Mickey's gloves and shoes, he typically wears only a pair of shorts with two large buttons in the front. Before Mickey was seen regularly in color animation, Mickey's shorts were either red or a dull blue-green. With the advent of Mickey's color films, the shorts were always red. When Mickey is not wearing his red shorts, he is often still wearing red clothing such as a red bandmaster coat
+  </p>
+
+  
+
    
 
 
 
 	<h2> Animation History </h2>
-    <p>
-     Disney had Ub Iwerks secretly begin animating a new cartoon while still under contract with Universal. The cartoon was co-directed by Walt Disney and Ub Iwerks. Iwerks was the main animator for the short and reportedly spent six weeks working on it. In fact, Iwerks was the main animator for every Disney short released in 1928 and 1929. Hugh Harman and Rudolf Ising also assisted Disney during those years. They had already signed their contracts with Charles Mintz, but he was still in the process of forming his new studio and so for the time being they were still employed by Disney. This short would be the last they animated under this somewhat awkward situation.[21]
-     Mickey was first seen in a test screening of the cartoon short Plane Crazy, on May 15, 1928, but it failed to impress the audience and to add insult to injury, Walt could not find a distributor. Though understandably disappointed, Walt went on to produce a second Mickey short, The Gallopin' Gaucho, which was also not released for lack of a distributor.
-     
-   </p>
+  <div>
 
-   <div>
-
-        <figure class="photo-frame">
+        <figure class="float">
           <img src="images/mouse3.png" alt="mickeymouse">
           <figcaption>Mickey</figcaption>
         </figure>
 
 
       </div>
+    <p>
+     Disney had Ub Iwerks secretly begin animating a new cartoon while still under contract with Universal. The cartoon was co-directed by Walt Disney and Ub Iwerks. Iwerks was the main animator for the short and reportedly spent six weeks working on it. In fact, Iwerks was the main animator for every Disney short released in 1928 and 1929. Hugh Harman and Rudolf Ising also assisted Disney during those years. They had already signed their contracts with Charles Mintz, but he was still in the process of forming his new studio and so for the time being they were still employed by Disney. This short would be the last they animated under this somewhat awkward situation.[21]
+     Mickey was first seen in a test screening of the cartoon short Plane Crazy, on May 15, 1928, but it failed to impress the audience and to add insult to injury, Walt could not find a distributor. Though understandably disappointed, Walt went on to produce a second Mickey short, The Gallopin' Gaucho, which was also not released for lack of a distributor.
+     
+   </p>
 
+   
   
 
    
@@ -94,7 +92,8 @@
       Mickey Mouse has received ten nominations for the Academy Award for Best Animated Short Film. These are Mickey's Orphans (1931), Building a Building (1933), Brave Little Tailor (1938), The Pointer (1939), Lend a Paw (1941), Squatter's Rights (1946), Mickey and the Seal (1948), Mickey's Christmas Carol (1983), Runaway Brain (1995), and Get a Horse! (2013). Among these, Lend a Paw was the only film to actually win the award. Additionally, in 1932 Walt Disney received an honorary Academy Award in recognition of Mickey's creation and popularity.
     </p>
 
-
+</div> <!-- container -->
+  </main>
  
 
 <!-- Footer -->
