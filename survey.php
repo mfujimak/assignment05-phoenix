ebdc90c993218f0a4d66be3f148579f3342ffc26
Valerie <xwang145@u.rochester.edu>
2018-04-23 16:23:18 -0400

    admin page styles updated

--- survey.php
+++ survey.php
@@ -14,13 +14,12 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 <head>
     <meta charset="UTF-8">
     <title>Sign Up</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+   <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <?php 
+        include 'includes/links.php'; 
+   ?> 
 </head>
-<body>
+<body class="admin">
     <?php 
         include 'includes/nav.php'; 
    ?>  
