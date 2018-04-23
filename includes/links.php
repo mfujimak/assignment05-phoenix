4f2071729b39bb68f1fa1d3743145e07c4ad085b
Valerie <xwang145@u.rochester.edu>
2018-04-23 15:51:15 -0400

    f and z pattern design

--- includes/links.php
+++ includes/links.php
@@ -1,9 +1,4 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
 <link rel="stylesheet" href="css/styles.css">
 <link href="https://fonts.googleapis.com/css?family=Norican" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Gaegu" rel="stylesheet">
-
-<!-- font-family: 'Norican', cursive; -->
-
-<!-- font-family: 'Raleway', sans-serif; -->
-<!-- font-family: 'Gaegu', cursive; -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
--- index.php
+++ index.php
@@ -10,30 +10,28 @@
  
 
 <header class="fixed">
-	<div class="container">
-		<div class="primary">
+	<div>
 			<a href="mickey.php"><img src="images/mickey_logo.png" alt="Mickey logo"></a>
-        </div>
-        <div class="secondary">
+    
     		<a href="stich.php"><img src="images/stich_logo.png" alt="Stich logo"></a>
-        </div>
     </div>
 		
 </header>
 
 <main>
 	<!-- Z-pattern and h1 should be in the center -->
-<h1> Welcome to the Disney World!</h1>
+	<div class="title">
+		<h1> Welcome to <br> the Disney World!</h1>
+	</div>
 
 </main>
 
-<footer class = "fixed">
+<footer>
 	<div class="container">
-		<div class="third">
+		<div class="first">
 	<a href="simba.php"><img src="images/simba_logo.png" alt="Simba logo"></a>
 		</div>
-		<div class="Fourth">
-			
+		<div class="second">
 			<a class="border" href="login.php">Vote for your Favorite Character</a>
 		</div>
 	</div>
